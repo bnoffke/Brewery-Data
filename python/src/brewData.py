060dd7feca9649ecf2d474d3cdc6e93823720492
Ben Noffke <bnoffke3790@gmail.com>
2022-11-01 10:30:46 -0500

    Refactoring code, looping API pages

--- python/src/brewData.py
+++ python/src/brewData.py
@@ -7,70 +7,86 @@ from pandas.io import gbq
 import datetime
 from dateutil import parser
 
-key_path = './brewery-data-local-machine-key.json'
+class BreweryLoader():
 
-# Construct a BigQuery client object.
-credentials = service_account.Credentials.from_service_account_file(
-    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
-)
+    def __init__(self) -> None:
+        self.key_path = './brewery-data-local-machine-key.json'
+        self.credentials = service_account.Credentials.from_service_account_file(
+            self.key_path, scopes=['https://www.googleapis.com/auth/cloud-platform'],
+            )
+        self.client = bigquery.Client(credentials=self.credentials, project=self.credentials.project_id,)
 
-#Start a client to connect to our BQ project
-client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
+        self.breweries = pd.DataFrame()
+    
+    def prepStagingTable(self,tableDefSql):
+        with open (tableDefSql,'r') as create_table_file:
+            create_table = create_table_file.read()
+        create_table_file.close()
+        query_job = self.client.query(create_table)
+        query_job.result()
+    
+    def getBreweries(self,urlBase):
+        #Prepare to pull data from API
+        url = urlBase + '&per_page=50'
+        page = 1
+        r = requests.get(url)
+        data = r.json()
+        #Loop through pages of API calls and append results to our dataframe
+        while len(data) > 0:
+            self.breweries = pd.concat([self.breweries,pd.DataFrame.from_dict(data)])
+            page += 1
+            r = requests.get(url + f'&page={page}')
+            data = r.json()
+    
+    def cleanBreweriesDataTypes(self):
+        colTypes = {'id': str,
+                    'name': str,
+                    'brewery_type': str,
+                    'street': str,
+                    'address_2': str,
+                    'address_3': str,
+                    'city': str,
+                    'state': str,
+                    'county_province': str,
+                    'postal_code': str,
+                    'country': str,
+                    'longitude': float,
+                    'latitude': float,
+                    'phone': str,
+                    'website_url': str
+                    }
+        cols = list(colTypes.keys())
+        strCols = [col for col in colTypes if colTypes[col] == str]
 
-#Grab the SQL to create the staging table
-with open ('./python/src/staging_breweries.sql','r') as create_table_file:
-    create_table = create_table_file.read()
-create_table_file.close()
-query_job = client.query(create_table)
-query_job.result()
+        self.breweries = self.breweries.astype(colTypes)
+        self.breweries.updated_at = self.breweries.updated_at.apply(parser.parse)
+        self.breweries.created_at = self.breweries.created_at.apply(parser.parse)
+        self.breweries[strCols] = self.breweries[strCols].replace('None','')
 
-#Prepare to pull data from API
-url = 'https://api.openbrewerydb.org/breweries?by_state=wisconsin&per_page=50'
-r = requests.get(url)
-data = r.json()
+    def loadBreweries(self,table_id):
+        #Prepare to load data into the staging table
+        #table_id = "brewery-data-367214.brewData.staging_breweries"
+        job_config = bigquery.LoadJobConfig(source_format = 'CSV')
 
-df = pd.DataFrame.from_dict(data)
+        job = self.client.load_table_from_dataframe(
+            self.breweries, table_id, job_config=job_config,
+        )
+        job.result()  # Wait for the job to complete.
 
-colTypes = {'id': str,
-            'name': str,
-            'brewery_type': str,
-            'street': str,
-            'address_2': str,
-            'address_3': str,
-            'city': str,
-            'state': str,
-            'county_province': str,
-            'postal_code': str,
-            'country': str,
-            'longitude': float,
-            'latitude': float,
-            'phone': str,
-            'website_url': str
-            }
-cols = list(colTypes.keys())
-strCols = [col for col in colTypes if colTypes[col] == str]
+        #Check what we loaded
+        table = self.client.get_table(table_id)
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), table_id
+            )
+        )
 
-df = df.astype(colTypes)
-df.updated_at = df.updated_at.apply(parser.parse)
-df.created_at = df.created_at.apply(parser.parse)
-df[strCols] = df[strCols].replace('None','')
+def main():
+    myBreweryLoader = BreweryLoader()
+    myBreweryLoader.getBreweries(urlBase = 'https://api.openbrewerydb.org/breweries?by_state=wisconsin')
+    myBreweryLoader.cleanBreweriesDataTypes()
+    myBreweryLoader.prepStagingTable(tableDefSql = './python/src/staging_breweries.sql')
+    myBreweryLoader.loadBreweries(table_id = 'brewery-data-367214.brewData.staging_breweries')
 
-#Prepare to load data into the staging table
-table_id = "brewery-data-367214.brewData.staging_breweries"
-job_config = bigquery.LoadJobConfig(
-  #write_disposition='WRITE_TRUNCATE',
-  source_format = 'CSV'
-)
-
-job = client.load_table_from_dataframe(
-    df, table_id, job_config=job_config,
-)
-job.result()  # Wait for the job to complete.
-
-#Check what we loaded
-table = client.get_table(table_id)
-print(
-    "Loaded {} rows and {} columns to {}".format(
-        table.num_rows, len(table.schema), table_id
-    )
-)
+if __name__ == '__main__':
+    main()
